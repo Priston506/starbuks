e070ee1e0025c95156040b93e4270a0da4647b0f
Priston506 <57559457+Priston506@users.noreply.github.com>
2021-03-23 15:42:17 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.Starbucks').src = anything;
+}
+
+function changeColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
